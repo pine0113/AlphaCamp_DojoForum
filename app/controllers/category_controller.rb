b0f8058eacd4ab524385aed5e90b3254f5acb169
sinash <pine0113@gmail.com>
2018-10-27 11:26:00 +0800

    fix not login post list

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,22 +2,42 @@ class CategoryController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
+    if current_user
     
-    case params["sort"]
-    when "view_count_up"
-      @posts =  @category.posts.can_view_by(current_user).sort_by_popularity('ASC').page(params[:page]).per(20)
-    when "view_count_down"
-      @posts =  @category.posts.can_view_by(current_user).sort_by_popularity('DESC').page(params[:page]).per(20)
-    when "reply_count_up"
-      @posts =  @category.posts.can_view_by(current_user).order("replies_count ASC").page(params[:page]).per(20)
-    when "reply_count_down"
-      @posts =  @category.posts.can_view_by(current_user).order("replies_count DESC").page(params[:page]).per(20)
-    when "reply_time_up"
-      @posts =  @category.posts.can_view_by(current_user).order("last_reply_time ASC").page(params[:page]).per(20)
-    when "reply_time_down"
-      @posts =  @category.posts.can_view_by(current_user).order("last_reply_time DESC").page(params[:page]).per(20)
+      case params["sort"]
+      when "view_count_up"
+        @posts =  @category.posts.can_view_by(current_user).sort_by_popularity('ASC').page(params[:page]).per(20)
+      when "view_count_down"
+        @posts =  @category.posts.can_view_by(current_user).sort_by_popularity('DESC').page(params[:page]).per(20)
+      when "reply_count_up"
+        @posts =  @category.posts.can_view_by(current_user).order("replies_count ASC").page(params[:page]).per(20)
+      when "reply_count_down"
+        @posts =  @category.posts.can_view_by(current_user).order("replies_count DESC").page(params[:page]).per(20)
+      when "reply_time_up"
+        @posts =  @category.posts.can_view_by(current_user).order("last_reply_time ASC").page(params[:page]).per(20)
+      when "reply_time_down"
+        @posts =  @category.posts.can_view_by(current_user).order("last_reply_time DESC").page(params[:page]).per(20)
+      else
+        @posts = @category.posts.can_view_by(current_user).page(params[:page]).per(20)
+      end
     else
-      @posts = @category.posts.can_view_by(current_user).page(params[:page]).per(20)
+      case params["sort"]
+      when "view_count_up"
+        @posts =  @category.posts.access_all.published.sort_by_popularity('ASC').page(params[:page]).per(20)
+      when "view_count_down"
+        @posts =  @category.posts.access_all.published.sort_by_popularity('DESC').page(params[:page]).per(20)
+      when "reply_count_up"
+        @posts =  @category.posts.access_all.published.order("replies_count ASC").page(params[:page]).per(20)
+      when "reply_count_down"
+        @posts =  @category.posts.access_all.published.order("replies_count DESC").page(params[:page]).per(20)
+      when "reply_time_up"
+        @posts =  @category.posts.access_all.published.order("last_reply_time ASC").page(params[:page]).per(20)
+      when "reply_time_down"
+        @posts =  @category.posts.access_all.published.order("last_reply_time DESC").page(params[:page]).per(20)
+      else
+        @posts = @category.posts.access_all.published.page(params[:page]).per(20)
+      end
+
     end
     @categories = Category.all
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,23 +3,44 @@ class PostsController < ApplicationController
   before_action :set_post, except: [:index, :new, :create]
 
   def index
-    case params["sort"]
-    when "view_count_up"
-      @posts = Post.can_view_by(current_user).sort_by_popularity('ASC').page(params[:page]).per(20)
-    when "view_count_down"
-      @posts = Post.can_view_by(current_user).sort_by_popularity('DESC').page(params[:page]).per(20)
-    when "reply_count_up"
-      @posts = Post.can_view_by(current_user).order("replies_count ASC").page(params[:page]).per(20)
-    when "reply_count_down"
-      @posts = Post.can_view_by(current_user).order("replies_count DESC").page(params[:page]).per(20)
-    when "reply_time_up"
-      @posts = Post.can_view_by(current_user).order("last_reply_time ASC").page(params[:page]).per(20)
-    when "reply_time_down"
-      @posts = Post.can_view_by(current_user).order("last_reply_time DESC").page(params[:page]).per(20)
+
+    if current_user  
+      case params["sort"]
+      when "view_count_up"
+        @posts = Post.can_view_by(current_user).sort_by_popularity('ASC').page(params[:page]).per(20)
+      when "view_count_down"
+        @posts = Post.can_view_by(current_user).sort_by_popularity('DESC').page(params[:page]).per(20)
+      when "reply_count_up"
+        @posts = Post.can_view_by(current_user).order("replies_count ASC").page(params[:page]).per(20)
+      when "reply_count_down"
+        @posts = Post.can_view_by(current_user).order("replies_count DESC").page(params[:page]).per(20)
+      when "reply_time_up"
+        @posts = Post.can_view_by(current_user).order("last_reply_time ASC").page(params[:page]).per(20)
+      when "reply_time_down"
+        @posts = Post.can_view_by(current_user).order("last_reply_time DESC").page(params[:page]).per(20)
+      else
+        @posts = Post.can_view_by(current_user).page(params[:page]).per(20)
+      end
     else
-      @posts = Post.can_view_by(current_user).page(params[:page]).per(20)
+       
+      case params["sort"]
+      when "view_count_up"
+        @posts = Post.access_all.published.sort_by_popularity('ASC').page(params[:page]).per(20)
+      when "view_count_down"
+        @posts = Post.access_all.published.sort_by_popularity('DESC').page(params[:page]).per(20)
+      when "reply_count_up"
+        @posts = Post.access_all.published.order("replies_count ASC").page(params[:page]).per(20)
+      when "reply_count_down"
+        @posts = Post.access_all.published.order("replies_count DESC").page(params[:page]).per(20)
+      when "reply_time_up"
+        @posts = Post.access_all.published.order("last_reply_time ASC").page(params[:page]).per(20)
+      when "reply_time_down"
+        @posts = Post.access_all.published.order("last_reply_time DESC").page(params[:page]).per(20)
+      else
+        @posts = Post.access_all.published.page(params[:page]).per(20)
+      end
     end
-    @categories = Category.all
+      @categories = Category.all
   end
 
   def new
