209cd081d4441774702ba04cb1b4840281378798
sinash <pine0113@gmail.com>
2018-11-05 06:37:25 +0800

    add admin nav

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181027092625) do
+ActiveRecord::Schema.define(version: 20181104222613) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
